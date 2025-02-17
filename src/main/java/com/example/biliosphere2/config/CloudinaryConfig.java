c0df8b5cbe3bc30ff151ddf8b3424775aebca031
ErlanPrambudi <erlanprambudi@gmail.com>
2025-02-17 16:57:05 +0700

    FIX PROJECT

---
+++ src/main/java/com/example/biliosphere2/config/CloudinaryConfig.java
@@ -0,0 +1,14 @@
+package com.example.biliosphere2.config;
+
+/*
+IntelliJ IDEA 2024.3 (Ultimate Edition)
+Build #IU-243.21565.193, built on November 13, 2024
+@Author Dell Erlan Prambudi
+Java Developer
+Created on 2/15/2025 7:10 AM
+@Last Modified 2/15/2025 7:10 AM
+Version 1.0
+*/
+
+public class ClaudinaryConfig {
+}
---
+++ src/main/java/com/example/biliosphere2/model/enums/StatusPeminjamanEnum.java
@@ -0,0 +1,15 @@
+package com.example.biliosphere2.model.enums;
+
+/*
+IntelliJ IDEA 2024.3 (Ultimate Edition)
+Build #IU-243.21565.193, built on November 13, 2024
+@Author Dell Erlan Prambudi
+Java Developer
+Created on 2/16/2025 4:23 AM
+@Last Modified 2/16/2025 4:23 AM
+Version 1.0
+*/
+
+public enum StatusPeminjaman {
+    DIAJUKAN, DISETUJUI, DITOLAK
+}
